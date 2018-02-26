07792a76457396c2613fd8a42b5640649d22276b
Cláudio Lemos <cflemos1999@icloud.com>
2018-02-26 00:08:36 +0000

    Task 5 terminada. Task 6 em desenvolvimento.

--- LPOO1718_T3G6/src/game/Map.java
+++ LPOO1718_T3G6/src/game/Map.java
@@ -1,9 +1,19 @@
 package game;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Map {
 	int lever = 0;
+	int heroX = 1;
+	int heroY = 1;
+	int guardX = 8;
+	int guardY = 1;
+	int ogreX = 4;
+	int ogreY = 1;
+	int level = 1;
+	int club = 0; 
 	
-	char map[][] = {
+	char map1[][] = {
 			{'X','X','X','X','X','X','X','X','X','X'},
 			{'X','H',' ',' ','I',' ','X',' ','G','X'},
 			{'X','X','X',' ','X','X','X',' ',' ','X'},
@@ -12,9 +22,31 @@ public class Map {
 			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
 			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
 			{'X','X','X',' ','X','X','X','X',' ','X'},
-			{'X',' ','I',' ','I',' ','X','K',' ','X'},
+			{'X',' ','I',' ','I',' ','X','k',' ','X'},
 			{'X','X','X','X','X','X','X','X','X','X'}
 	};
+	
+	char map2[][] = {
+			{'X','X','X','X','X','X','X','X','X','X'},
+			{'I',' ',' ',' ','O',' ',' ',' ','k','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X','H',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X','X','X','X','X','X','X','X','X','X'}
+	};
+	
+	char map[][] = map1;
+	
+	public void changeMap() {
+		map = map2;
+		heroX = 1;
+		heroY = 8;
+		level++;
+	}
 
 	public void printMap() {
 		for(int i = 0; i < 10; i++) {
@@ -25,103 +57,556 @@ public class Map {
 		}
 	}
 
-	public int moveLeft(char person, Integer x, Integer y) {
-		if (map[y][x-1] == 'X' || map[y][x-1] == 'I')
+	public int moveLeft(char person) {
+		int x = 0;
+		int y = 0;
+		
+		if(person == 'H') {
+			x = heroX;
+			y = heroY;
+		}
+		else if(person == 'G') {
+			x = guardX;
+			y = guardY;
+		}
+		else if(person == 'O') {
+			x = ogreX;
+			y = ogreY;
+		}
+		
+		if(person == 'O') {
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I')
+				map[y-1][x] = ' ';
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I')
+				map[y][x+1] = ' ';
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I')
+				map[y+1][x] = ' ';
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I')
+				map[y][x-1] = ' '; 
+		}
+		
+		if (map[y][x-1] == 'X') {
 			return 0;
+		}
+		else if (map[y][x-1] == 'I' && map[y][x] == 'K') {
+			if (lever == 1){
+				map[y][x-1] = 'S';
+				map[y][x] = 'H';
+			}
+			
+		}
 		else if (map[y][x-1] == ' ') {
-			map[y][x] = ' ';
-			map[y][x-1] = person;
+			if(map[y][x] == '$') {
+				map[y][x] = 'k';
+				map[y][x-1] = 'O';
+			}
+			else if (lever == 0 || person == 'O' || person == 'G') {
+				map[y][x - 1] = person;
+				map[y][x] = ' ';
+			}
+			else {
+				if (level == 2) {
+					map[y][x - 1] = 'K';
+				}
+				else {
+					map[y][x - 1] = 'H';
+				}
+
+				map[y][x] = ' ';
+			}
+			
+			if (lever == 1 && person == 'H' && level == 1) {
+				map[y][x] = 'k';
+				lever = 0;
+			}
 			x--;
 		}
-		else if (map[y][x-1] == 'K') {
+		else if (map[y][x-1] == 'k') {
 			map[y][x] = ' ';
-			map[y][x-1] = person;
+			if (level == 1) {
+				lever = 1;
+				map[y][x-1] = 'H';
+				map[5][0] = 'S';
+				map[6][0] = 'S';
+			}
+			else if (level == 2) {
+				if (person == 'H') {
+					lever = 1;
+					map[y][x-1] = 'K';
+				}
+				else if (person == 'O')
+					map[y][x-1] = '$';
+			}	
 			x--;
-			lever = 1;
-			map[5][0] = 'S';
-			map[6][0] = 'S';
 		}
-		else if (map[y][x-1] == 'S') {
+		else if (map[y][x-1] == 'S' && map[y][x] == 'H') {
 			map[y][x] = ' ';
 			map[y][x-1] = person;
 			return 1;
 		}
 		
-		if (map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G') {
+		if(level == 2 && person == 'O') {
+			club = ThreadLocalRandom.current().nextInt(1, 5);
+			
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I') {
+				if (map[y-1][x] == 'k') {
+					map[y-1][x] = '$';
+				}
+				else
+					map[y-1][x] = '*';
+			}
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I') {
+				if (map[y][x+1] == 'k') {
+					map[y][x+1] = '$';
+				}
+				else
+					map[y][x+1] = '*';
+			}
+			else if(club == 3 && map[y+1][x] != 'X'&& map[y+1][x] != 'I') {
+				if (map[y+1][x] == 'k') {
+					map[y+1][x] = '$';
+				}
+				else
+					map[y+1][x] = '*';
+			}
+			else if(club == 4 && map[y][y-1] != 'X'&& map[y][x-1] != 'I') {
+				if (map[y][x-1] == 'k') {
+					map[y][x-1] = '$';
+				}
+				else
+					map[y][x-1] = '*';
+			}
+		}
+		
+		if (map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G' || map[y][x] == '*') {
 			return -1;
 		}
+		
+		if(person == 'H') {
+			heroX = x;
+			heroY = y;
+		}
+		else if(person == 'G') {
+			guardX = x;
+			guardY = y;
+		}
+		else if(person == 'O') {
+			ogreX = x;
+			ogreY = y;
+		}
+		
 		
 		return 0;
 	}
 	
-	public int moveRight(char person, Integer x, Integer y) {
-		if (map[y][x+1] == ' ') {
-			map[y][x] = ' ';
-			map[y][x+1] = person;
+	public int moveRight(char person) {
+		int x = 0;
+		int y = 0;
+		
+		if(person == 'H') {
+			x = heroX;
+			y = heroY;
+		}
+		else if(person == 'G') {
+			x = guardX;
+			y = guardY;
+		}
+		else if(person == 'O') {
+			x = ogreX;
+			y = ogreY;
+		}
+		
+		if(person == 'O') {
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I')
+				map[y-1][x] = ' ';
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I')
+				map[y][x+1] = ' ';
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I')
+				map[y+1][x] = ' ';
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I')
+				map[y][x-1] = ' '; 
+		}
+		
+		if (map[y][x+1] == 'X')
+			return 0;
+		else if (map[y][x+1] == 'I') {
+			if (lever == 1 && map[y][x] == 'K'){
+				map[y][x+1] = 'S';
+				map[y][x] = 'H';
+			}
+		}
+		else if (map[y][x+1] == ' ') {
+			if(map[y][x] == '$') {
+				map[y][x] = 'k';
+				map[y][x+1] = 'O';
+			}
+			else if (lever == 0 || person == 'O' || person == 'G') {
+				map[y][x+1] = person;
+				map[y][x] = ' ';
+			}
+			else {
+				if (level == 2) {
+					map[y][x+1] = 'K';
+				}
+				else {
+					map[y][x+1] = 'H';
+				}
+				map[y][x] = ' ';
+			}
+			if (lever == 1 && person == 'H' && level == 1) {
+				map[y][x] = 'k';
+				lever = 0;
+			}
 			x++;
 		}
-		else if (map[y][x+1] == 'G') {
+		else if (map[y][x+1] == 'k') {
+			map[y][x] = ' ';
+			if (level == 1) {
+				lever = 1;
+				map[y][x+1] = 'H';
+				map[5][0] = 'S';
+				map[6][0] = 'S';
+			}
+			else if (level == 2) {
+				if (person == 'H') {
+					lever = 1;
+					map[y][x+1] = 'K';
+				}
+				else if (person == 'O')
+					map[y][x+1] = '$';
+			}
+			x++;
+		}
+		else if (map[y][x+1] == 'S' && map[y][x] == 'H') {
+			map[y][x] = ' ';
+			map[y][x+1] = person;
+			return 1;
+		}
+		
+		if(level == 2 && person == 'O') {
+			club = ThreadLocalRandom.current().nextInt(1, 5);
 			
-		}
-		else if (map[y][x+1] == 'K') {
-//			map[y][x] = ' ';
-//			map[y][x-1] = person;
-//			x--;
-//			kex = 1;
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I') {
+				if (map[y-1][x] == 'k') {
+					map[y-1][x] = '$';
+				}
+				else
+					map[y-1][x] = '*';
+			}
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I') {
+				if (map[y][x+1] == 'k') {
+					map[y][x+1] = '$';
+				}
+				else
+					map[y][x+1] = '*';
+			}
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I') {
+				if (map[y+1][x] == 'k') {
+					map[y+1][x] = '$';
+				}
+				else
+					map[y+1][x] = '*';
+			}
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I') {
+				if (map[y][x-1] == 'k') {
+					map[y][x-1] = '$';
+				}
+				else
+					map[y][x-1] = '*';
+			}
 		}
 		
-		if (lever == 1) {
-			map[y][x-1] = 'K';
-		}
-		
-		if (map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G') {
+		if (map[y][x+1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G' || map[y][x] == '*') {
 			return -1;
+		}
+		
+		if(person == 'H') {
+			heroX = x;
+			heroY = y;
+		}
+		else if(person == 'G') {
+			guardX = x;
+			guardY = y;
+		}
+		else if(person == 'O') {
+			ogreX = x;
+			ogreY = y;
 		}
 		
 		return 0;
 	}
 	
-	public int moveUp(char person, Integer x, Integer y) {
-		if (map[y-1][x] == ' ') {
-			map[y][x] = ' ';
-			map[y-1][x] = person;
-			y--;
+	public int moveDown(char person) {
+		int x = 0;
+		int y = 0;
+		
+		if(person == 'H') {
+			x = heroX;
+			y = heroY;
 		}
-		else if (map[y-1][x] == 'G') {
-			
+		else if(person == 'G') {
+			x = guardX;
+			y = guardY;
 		}
-		else if (map[y-1][x] == 'K') {
-//			map[y][x] = ' ';
-//			map[y][x-1] = person;
-//			x--;
-//			kex = 1;
+		else if(person == 'O') {
+			x = ogreX;
+			y = ogreY;
 		}
 		
-		if (map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G') {
-			return -1;
+		if(person == 'O') {
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I')
+				map[y-1][x] = ' ';
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I')
+				map[y][x+1] = ' ';
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I')
+				map[y+1][x] = ' ';
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I')
+				map[y][x-1] = ' '; 
 		}
 		
-		return 0;
-	}
-	
-	public int moveDown(char person, Integer x, Integer y) {
-		if (map[y+1][x] == ' ') {
-			map[y][x] = ' ';
-			map[y+1][x] = person;
+		if (map[y+1][x] == 'X')
+			return 0;
+		else if (map[y+1][x] == 'I') {
+			if (lever == 1 && map[y][x] == 'K'){
+				map[y+1][x] = 'S';
+				map[y][x] = 'H';
+			}
+		}
+		else if (map[y+1][x] == ' ') {
+			if(map[y][x] == '$') {
+				map[y][x] = 'k';
+				map[y+1][x] = 'O';
+			}
+			else if (lever == 0 || person == 'O' || person == 'G') {
+				map[y+1][x] = person;
+				map[y][x] = ' ';
+			}
+			else {
+				if (level == 2) {
+					map[y+1][x] = 'K';
+				}
+				else {
+					map[y+1][x] = 'H';
+				}
+				map[y][x] = ' ';
+			}
+			if (lever == 1 && person == 'H' && level == 1) {
+				map[y][x] = 'k';
+				lever = 0;
+			}
 			y++;
 		}
-		else if (map[y+1][x] == 'G') {
-			
+		else if (map[y+1][x] == 'k') {
+			map[y][x] = ' ';
+			if (level == 1) {
+				lever = 1;
+				map[y+1][x] = 'H';
+				map[5][0] = 'S';
+				map[6][0] = 'S';
+			}
+			else if (level == 2) {
+				if (person == 'H') {
+					lever = 1;
+					map[y+1][x] = 'K';
+				}
+				else if (person == 'O')
+					map[y+1][x] = '$';
+			}
+			y++;
 		}
-		else if (map[y+1][x] == 'K') {
-//			map[y][x] = ' ';
-//			map[y][x-1] = person;
-//			x--;
-//			keyd = 1;
+		else if (map[y+1][x] == 'S' && map[y][x] == 'H') {
+			map[y][x] = ' ';
+			map[y+1][x] = person;
+			return 1;
 		}
 		
-		if (map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y-1][x] == 'G' || map[y][x+1] == 'G') {
+		if(level == 2 && person == 'O') {
+			club = ThreadLocalRandom.current().nextInt(1, 5);
+			
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I') {
+				if (map[y-1][x] == 'k') {
+					map[y-1][x] = '$';
+				}
+				else
+					map[y-1][x] = '*';
+			}
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I') {
+				if (map[y][x+1] == 'k') {
+					map[y][x+1] = '$';
+				}
+				else
+					map[y][x+1] = '*';
+			}
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I') {
+				if (map[y+1][x] == 'k') {
+					map[y+1][x] = '$';
+				}
+				else
+					map[y+1][x] = '*';
+			}
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I') {
+				if (map[y][x-1] == 'k') {
+					map[y][x-1] = '$';
+				}
+				else
+					map[y][x-1] = '*';
+			}
+		}
+		
+		if (map[y+1][x] == 'G' || map[y][x+1] == 'G' || map[y][x-1] == 'G' || map[y+1][x] == 'G' || map[y][x] == '*') {
 			return -1;
+		}
+		
+		if(person == 'H') {
+			heroX = x;
+			heroY = y;
+		}
+		else if(person == 'G') {
+			guardX = x;
+			guardY = y;
+		}
+		else if(person == 'O') {
+			ogreX = x;
+			ogreY = y;
+		}
+		
+		return 0;
+	}
+	
+	public int moveUp(char person) {
+		int x = 0;
+		int y = 0;
+		
+		if(person == 'H') {
+			x = heroX;
+			y = heroY;
+		}
+		else if(person == 'G') {
+			x = guardX;
+			y = guardY;
+		}
+		else if(person == 'O') {
+			x = ogreX;
+			y = ogreY;
+		}
+		
+		if(person == 'O') {
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I')
+				map[y-1][x] = ' ';
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I')
+				map[y][x+1] = ' ';
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I')
+				map[y+1][x] = ' ';
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I')
+				map[y][x-1] = ' '; 
+		}
+		
+		if (map[y-1][x] == 'X')
+			return 0;
+		else if (map[y-1][x] == 'I') {
+			if (lever == 1 && map[y][x] == 'K'){
+				map[y-1][x] = 'S';
+				map[y][x] = 'H';
+			}
+		}
+		else if (map[y-1][x] == ' ') {
+			if(map[y][x] == '$') {
+				map[y][x] = 'k';
+				map[y-1][x] = 'O';
+			}
+			else if (lever == 0 || person == 'O' || person == 'G') {
+				map[y-1][x] = person;
+				map[y][x] = ' ';
+			}
+			else {
+				if (level == 2) {
+					map[y-1][x] = 'K';
+				}
+				else {
+					map[y-1][x] = 'H';
+				}
+				
+
+				map[y][x] = ' ';
+			}
+			if (lever == 1 && person == 'H' && level == 1) {
+				map[y][x] = 'k';
+				lever = 0;
+			}
+			y--;
+		}
+		else if (map[y-1][x] == 'k') {
+			map[y][x] = ' ';
+			if (level == 1) {
+				lever = 1;
+				map[y-1][x] = 'H';
+				map[5][0] = 'S';
+				map[6][0] = 'S';
+			}
+			else if (level == 2) {
+				if (person == 'H') {
+					lever = 1;
+					map[y-1][x] = 'K';
+				}
+				else if (person == 'O')
+					map[y-1][x] = '$';
+			}
+			y--;
+		}
+		else if (map[y-1][x] == 'S' && map[y][x] == 'H') {
+			map[y][x] = ' ';
+			map[y-1][x] = person;
+			return 1;
+		}
+		
+		if(level == 2 && person == 'O') {
+			club = ThreadLocalRandom.current().nextInt(1, 5);
+			
+			if(club == 1 && map[y-1][x] != 'X' && map[y-1][x] != 'I') {
+				if (map[y-1][x] == 'k') {
+					map[y-1][x] = '$';
+				}
+				else
+					map[y-1][x] = '*';
+			}
+			else if(club == 2 && map[y][x+1] != 'X' && map[y][x+1] != 'I') {
+				if (map[y][x+1] == 'k') {
+					map[y][x+1] = '$';
+				}
+				else
+					map[y][x+1] = '*';
+			}
+			else if(club == 3 && map[y+1][x] != 'X' && map[y+1][x] != 'I') {
+				if (map[y+1][x] == 'k') {
+					map[y+1][x] = '$';
+				}
+				else
+					map[y+1][x] = '*';
+			}
+			else if(club == 4 && map[y][x-1] != 'X' && map[y][x-1] != 'I') {
+				if (map[y][x-1] == 'k') {
+					map[y][x-1] = '$';
+				}
+				else
+					map[y][x-1] = '*';
+			}
+		}
+		
+		if (map[y-1][x] == 'G' || map[y][x-1] == 'G' || map[y][x+1] == 'G' || map[y-1][x] == 'G' || map[y-1][x] == '*') {
+			return -1;
+		}
+		
+		if(person == 'H') {
+			heroX = x;
+			heroY = y;
+		}
+		else if(person == 'G') {
+			guardX = x;
+			guardY = y;
+		}
+		else if(person == 'O') {
+			ogreX = x;
+			ogreY = y;
 		}
 		
 		return 0;
--- LPOO1718_T3G6/src/game/Start.java
+++ LPOO1718_T3G6/src/game/Start.java
@@ -1,52 +1,74 @@
 package game;
 import game.Map;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Start {
 	public static void main(String[] args) {
 		Map gameMap = new Map();
 		int gameState = 0;
+		int level = 1;
 		char input;
 		Scanner scanner = new Scanner(System.in);
 		gameMap.printMap();
 		char patrol[] = {'a','s','s','s','s','a','a','a','a','a','a','s','d','d','d','d','d','d','d','w','w','w','w','w','w'};
-		Integer heroX = 1;
-		Integer heroY = 1;
-		Integer guardX = 1;
-		Integer guardY = 1;
+		char ogre[] = {'a', 's', 'd', 'w'};
 		int indiceG = 0;
+		int indiceO = 0;
 		
 		while (gameState != -1) {
+			if (level == 1) {
+				if (patrol[indiceG] == 'w')
+					gameMap.moveUp('G');
+				else if (patrol[indiceG] == 'a')
+					gameMap.moveLeft('G');
+				else if (patrol[indiceG] == 's')
+					gameMap.moveDown('G');
+				else if (patrol[indiceG] == 'd')
+					gameMap.moveRight('G');
+			}
+			else if(level == 2) {
+				indiceO = ThreadLocalRandom.current().nextInt(0, 4);
+				if (ogre[indiceO] == 'w')
+					gameMap.moveUp('O');
+				else if (ogre[indiceO] == 'a')
+					gameMap.moveLeft('O');
+				else if (ogre[indiceO] == 's')
+					gameMap.moveDown('O');
+				else if (ogre[indiceO] == 'd')
+					gameMap.moveRight('O');
+				
+				System.out.println(indiceO);
+			}
 			input = scanner.next().charAt(0);
+			
 			if(input == 'w')
-				gameState = gameMap.moveUp('H', heroX, heroY);
+				gameState = gameMap.moveUp('H');
 			else if(input == 'a')
-				gameState = gameMap.moveLeft('H', heroX, heroY);
+				gameState = gameMap.moveLeft('H');
 			else if(input == 's')
-				gameState = gameMap.moveDown('H', heroX, heroY);
+				gameState = gameMap.moveDown('H');
 			else if(input == 'd')
-				gameState = gameMap.moveRight('H', heroX, heroY);
+				gameState = gameMap.moveRight('H');
+
 			
-			System.out.println("You fsvds!!");
-			if(patrol[indiceG] == 'w')
-				gameMap.moveUp('H', guardX, guardY);
-			else if(input == 'a')
-				gameMap.moveLeft('H', guardX, guardY);
-			else if(patrol[indiceG] == 's')
-				gameMap.moveDown('H', guardX, guardY);
-			else if(patrol[indiceG] == 'd')
-				gameMap.moveRight('H', guardX, guardY);
-			
-			System.out.println("You dsfdsgd!!");
 			indiceG++;
 			if (indiceG == patrol.length)
 				indiceG = 0;
 			
-			gameMap.printMap();
-			System.out.println("You hgjygj!!");
+			if (indiceO == ogre.length)
+				indiceO = 0;
+			
 			
 			if (gameState == 1) {
+				System.out.println("Next Level!!");
+				gameMap.changeMap();
+				gameState = 0;
+				level++;
+			}
+			gameMap.printMap();
+			if (level == 3) {
 				System.out.println("You win!!");
 				return;
 			}
