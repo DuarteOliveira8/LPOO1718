d96064a8f3c7047ceab44c9d90c0706931ee5eec
Cláudio Lemos <cflemos1999@icloud.com>
2018-06-04 00:26:56 +0100

    Organization

--- Final Project/New Folder With Items/android/build/intermediates/assets/debug/cityFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cityFloor" tilewidth="10" tileheight="10" tilecount="12288" columns="192">
- <image source="cityFloor.png" width="1920" height="640"/>
-</tileset>
--- Final Project/New Folder With Items/android/build/intermediates/assets/debug/darkForestFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="darkForestFloor" tilewidth="10" tileheight="10" tilecount="12288" columns="192">
- <image source="darkForestFloor.png" width="1920" height="640"/>
-</tileset>
