46d14d945fb018d46b466fff478b63775996b99e
Cláudio Lemos <cflemos1999@icloud.com>
2018-06-03 23:57:02 +0100

    Organization

---
+++ Final Project/New Folder With Items/android/build/intermediates/assets/debug/lightForestFloor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lightForestFloor" tilewidth="10" tileheight="10" tilecount="12288" columns="192">
+ <image source="lightForestFloor.png" width="1920" height="640"/>
+</tileset>
